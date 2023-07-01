da509178b191e15b679c009834227d329e34090c
seongm2n <ssongpig@naver.com>
2023-07-01 23:07:14 +0900

    feat : 로컬스토리지에 todo리스트를 저장할 수 있는 기능 추가

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 function TodoList({ status }) {
-	const [todos, setTodos] = useState([
-		{ id: '123', text: '카페가기', status: 'active' },
-		{ id: '124', text: '공부하기', status: 'active' },
-		{ id: '125', text: '맛있는거 먹기', status: 'active' },
-	]);
+	const [todos, setTodos] = useState([]);
+
+	const saveTodosToLocalStorage = (todos) => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	};
 
 	const handleAdd = (todo) => {
 		setTodos([...todos, todo]);
+		saveTodosToLocalStorage([...todos, todo]);
 	};
 	const handleUpdate = (updated) => {
 		setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+		saveTodosToLocalStorage(
+			todos.map((t) => (t.id === updated.id ? updated : t))
+		);
 	};
 	const handleDelete = (deleted) => {
 		setTodos(todos.filter((t) => t.id !== deleted.id));
+		saveTodosToLocalStorage(todos.filter((t) => t.id !== deleted.id));
 	};
+
+	useEffect(() => {
+		const storedTodos = localStorage.getItem('todos');
+		if (storedTodos) {
+			setTodos(JSON.parse(storedTodos));
+		}
+	}, []);
 
 	const statused = getStatusedItems(todos, status);
 	return (
-		<section className={styles.container} >
+		<section className={styles.container}>
 			<ul className={styles.list}>
 				{statused.map((todo) => (
 					<Todo
