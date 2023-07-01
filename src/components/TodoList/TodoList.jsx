37e767e11c838b2e5d19ce1c2d82b36f7da48028
seongm2n <ssongpig@naver.com>
2023-07-01 23:38:06 +0900

    fix : local storage 수정

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,33 +4,23 @@ import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 function TodoList({ status }) {
-	const [todos, setTodos] = useState([]);
-
-	const saveTodosToLocalStorage = (todos) => {
-		localStorage.setItem('todos', JSON.stringify(todos));
-	};
-
+	// 인자와 호출하는 내용이 동일하다면 함수의 이름(참조값)만 전달해도 같음
+	// useState(초기화하는함수) -> 함수의 레퍼런스만 전달
+	// useState(()=>초기화하는함수())->위와 동일함, 단 콜백함수르 만듦(단점을 꼽자면, 불필요한 함수가 만들어진다는 단점)
+	const [todos, setTodos] = useState(readTodosFormLocalStorage);
 	const handleAdd = (todo) => {
 		setTodos([...todos, todo]);
-		saveTodosToLocalStorage([...todos, todo]);
 	};
 	const handleUpdate = (updated) => {
 		setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
-		saveTodosToLocalStorage(
-			todos.map((t) => (t.id === updated.id ? updated : t))
-		);
 	};
 	const handleDelete = (deleted) => {
 		setTodos(todos.filter((t) => t.id !== deleted.id));
-		saveTodosToLocalStorage(todos.filter((t) => t.id !== deleted.id));
 	};
 
 	useEffect(() => {
-		const storedTodos = localStorage.getItem('todos');
-		if (storedTodos) {
-			setTodos(JSON.parse(storedTodos));
-		}
-	}, []);
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
 	const statused = getStatusedItems(todos, status);
 	return (
@@ -50,6 +40,11 @@ function TodoList({ status }) {
 	);
 }
 export default TodoList;
+
+function readTodosFormLocalStorage() {
+	const todos = localStorage.getItem('todos');
+	return todos ? JSON.parse(todos) : [];
+}
 
 function getStatusedItems(todos, status) {
 	if (status === 'all') {
